f511aacf9ead0e2b6604e586e3fe924f85ce9a5c
Mackenzie <kenziedy@students.cs.ubc.ca>
2025-09-03 19:48:06 -0700

    ~ added logging

--- bot/loop.py
+++ bot/loop.py
@@ -2,6 +2,7 @@ from .elevator import wait_for_stop
 from .actions import visit_zach, exit, visit_kmsf, continue_button
 
 def run():
+    runs = 0
     while True:
         visit_kmsf()
         wait_for_stop()
@@ -9,3 +10,6 @@ def run():
         visit_zach()
         continue_button()
         exit()
+
+        runs += 1
+        print(f"Completed {runs} elevator runs.")
